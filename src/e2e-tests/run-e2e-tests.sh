2a172e50020afcd55439a8677938cf8d4044fbe0
Matt Zeunert <matt@mostlystatic.com>
2017-05-01 20:10:44 +0800

    Wait for selenium server to be started.

--- src/e2e-tests/run-e2e-tests.sh
+++ src/e2e-tests/run-e2e-tests.sh
@@ -1,8 +1,9 @@
 trap 'jobs -p | xargs kill' SIGINT SIGTERM EXIT
 # We don't use protractor, but webdriver-manager make it easy
 # to install webdriver/chromedriver
+./node_modules/.bin/webdriver-manager start &
 ./node_modules/.bin/http-server -p 7888 &
 node ./bin/glasswing.js --save /tmp/glasswing &
-./node_modules/.bin/webdriver-manager start &
+sleep 5 # wait for selenium server to be started by webdriver-manager
 echo $(jobs -p)
 ./node_modules/.bin/wdio wdio.conf.js
