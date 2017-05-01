66d33875c2e302aab799cbd8cdd8b30ac7e39bcc
Matt Zeunert <matt@mostlystatic.com>
2017-05-01 20:07:58 +0800

    Merge.

--- src/e2e-tests/run-e2e-tests.sh
+++ src/e2e-tests/run-e2e-tests.sh
@@ -1,9 +1,11 @@
 trap 'jobs -p | xargs kill' SIGINT SIGTERM EXIT
+# delete existing recordings if any
+[ -e /tmp/glasswing-e2e-test ] && rm -r /tmp/glasswing-e2e-test
 # We don't use protractor, but webdriver-manager make it easy
 # to install webdriver/chromedriver
 ./node_modules/.bin/webdriver-manager start &
 ./node_modules/.bin/http-server -p 7888 &
-node ./bin/glasswing.js --save /tmp/glasswing &
+node ./bin/glasswing.js --save /tmp/glasswing-e2e-test &
 sleep 5 # wait for selenium server to be started by webdriver-manager
 echo $(jobs -p)
 ./node_modules/.bin/wdio wdio.conf.js
