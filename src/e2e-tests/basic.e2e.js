65160150bb563a5db8757c7bc2966dd38eff4dfb
Matt Zeunert <matt@mostlystatic.com>
2017-05-01 20:20:22 +0800

    Check file listing loads in E2E test.

--- src/e2e-tests/basic.e2e.js
+++ src/e2e-tests/basic.e2e.js
@@ -8,13 +8,21 @@ describe("Basic Test", function(){
                 return window.a
             }).value
             return a == 25;
-        }, 30000, 'expected a to be set to 25 by the page')
+        }, 20000, 'expected a to be set to 25 by the page')
         browser.waitUntil(function () {
             var a = browser.execute(function(){
                 return window.__jscb.recordedValueBuffer.length
             }).value
             console.log("length", a)
             return a == 0;
-        }, 30000, 'expected values to be sent to server and value buffer to be reset');
+        }, 20000, 'expected values to be sent to server and value buffer to be reset');
+    })
+    it("Shows simple.js in the Glasswing file listing", function(){
+        browser.url('http://localhost:9500')
+        var fileLinksTable = $(".file-links")
+        fileLinksTable.waitForExist(5000)
+        var jsFilePath = "http://localhost:7888/src/e2e-tests/basic-demo-page/basic-demo.js"
+        assert(fileLinksTable.getText().indexOf(jsFilePath) !== -1)
+    })
     })
 })
